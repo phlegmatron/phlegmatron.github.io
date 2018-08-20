94be1dca91e63b5afba6c2ef3578e7cc7d25da9d
Станислав <36840910+phlegmatron@users.noreply.github.com>
2018-08-21 00:04:48 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,78 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d")
-var mouseOver
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+    mainElement = new component(0, 550, 50, 50, 10);
+    dropdownElement = new component(150, 0, 30, 30, 5)
+    keyState = {}
+    score = 0;
 
-canvas.onmouseover = () => {
-  mouseOver = true;
-}
-
-canvas.onmouseout = () => {
-  mouseOver = false;
-}
-
-document.onmousedown = (e) => {
-  ctx.moveTo(e.offsetX, e.offsetY)
-  document.onmousemove = (e) => {
-    if (mouseOver) {
-      ctx.lineTo(e.offsetX, e.offsetY)
-      console.log(e.offsetX)
-      ctx.stroke();
+function component(x, y, width, height, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.width = width;
+  this.height = height;
+  this.update = () => {
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+  }
+  this.crashWith = (otherobj) => {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var crash = true;
+    if ((mybottom < othertop) ||
+           (mytop > otherbottom) ||
+           (myright < otherleft) ||
+           (myleft > otherright)) {
+       crash = false;
     }
-  }
-  document.onmouseup = () => {
-    document.onmousemove = null;
-  }
+    return crash;
+}
 }
 
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
+function clearFrame() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
+
+function drawFrame() {
+  clearFrame();
+  if (keyState[37] && mainElement.x > 0) mainElement.x -= mainElement.speed
+  if (keyState[39] && mainElement.x < (canvas.width - mainElement.width) ) mainElement.x += mainElement.speed
+  if (dropdownElement.y === -dropdownElement.height) dropdownElement.x = getRandomNumber(0, canvas.width - dropdownElement.width);
+  dropdownElement.y += dropdownElement.speed
+  if ((dropdownElement.y + dropdownElement.height) > canvas.height) {
+    alert('bam')
+    document.location.reload();
+    return;
+  }
+  if (mainElement.crashWith(dropdownElement)) {
+    dropdownElement.y = -dropdownElement.height;
+    score++
+    dropdownElement.speed += score / 100
+  }
+  ctx.font = '20px Arial'
+  ctx.fillText(score, 350, 20)
+  mainElement.update();
+  dropdownElement.update();
+  requestAnimationFrame(drawFrame);
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+window.onkeydown = (event) => {
+  keyState[event.keyCode] = true;
+}
+window.onkeyup = (event) => {
+  keyState[event.keyCode] = false;
+}
+
+drawFrame();
+
+
+
