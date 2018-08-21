99d0dc7558ef86b0236928580f553d7ac217e549
Станислав <36840910+phlegmatron@users.noreply.github.com>
2018-08-21 14:21:06 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
-    mainElement = new component(0, 550, 50, 50, 10);
-    dropdownElement = new component(150, 0, 30, 30, 5)
+    mainElement = new animation(0, 500, 150, 150, 20);
+    dropdownElement = new component(150, 0, 50, 50, 5)
     keyState = {}
     score = 0;
+
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+window.onkeydown = (event) => {
+  keyState[event.keyCode] = true;
+}
+window.onkeyup = (event) => {
+  keyState[event.keyCode] = false;
+}
 
 function component(x, y, width, height, speed) {
   this.x = x;
@@ -11,8 +21,10 @@ function component(x, y, width, height, speed) {
   this.speed = speed;
   this.width = width;
   this.height = height;
+  this.image = new Image;
+  this.image.src = './img/beer.jpeg';
   this.update = () => {
-    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
   }
   this.crashWith = (otherobj) => {
     var myleft = this.x;
@@ -31,7 +43,51 @@ function component(x, y, width, height, speed) {
        crash = false;
     }
     return crash;
+  }
 }
+
+function animation(x, y, width, height, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.width = width;
+  this.height = height;
+  this.frame = 0;
+  this.frameSize = 125;
+  this.frameX = 0;
+  this.frameY = 0;
+  this.frameCount = 0;
+  var spriteSheet = new Image;
+  spriteSheet.src = './img/move-animation.jpg'
+  this.image = spriteSheet;
+  this.update = () => {
+    if (this.frameX > 3) {
+      this.frameX = 0;
+      this.frameY++
+    }
+    if (this.frameY > 1) {
+      this.frameY = 0;
+    }
+    ctx.drawImage(this.image, (this.frameX * this.frameSize), (this.frameY * this.frameSize), this.frameSize, this.frameSize, this.x, this.y, this.width, this.height)
+  }
+  this.crashWith = (otherobj) => {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var crash = true;
+    if ((mybottom < othertop) ||
+           (mytop > otherbottom) ||
+           (myright < otherleft) ||
+           (myleft > otherright)) {
+       crash = false;
+    }
+    return crash;
+  }
 }
 
 function clearFrame() {
@@ -40,12 +96,18 @@ function clearFrame() {
 
 function drawFrame() {
   clearFrame();
-  if (keyState[37] && mainElement.x > 0) mainElement.x -= mainElement.speed
-  if (keyState[39] && mainElement.x < (canvas.width - mainElement.width) ) mainElement.x += mainElement.speed
+  if (keyState[37] && mainElement.x > 0) {
+    mainElement.x -= mainElement.speed;
+    mainElement.frameX++;
+  }
+  if (keyState[39] && mainElement.x < (canvas.width - mainElement.width) ) {
+    mainElement.x += mainElement.speed;
+    mainElement.frameX++;
+  }
   if (dropdownElement.y === -dropdownElement.height) dropdownElement.x = getRandomNumber(0, canvas.width - dropdownElement.width);
   dropdownElement.y += dropdownElement.speed
   if ((dropdownElement.y + dropdownElement.height) > canvas.height) {
-    var gameOver = confirm('Игра окончена, еще раз?')
+    var gameOver = confirm(`Игра окончена, ваш счёт: ${score}. Еще раз?`)
     if (gameOver) document.location.reload();
     return;
   }
@@ -55,7 +117,7 @@ function drawFrame() {
     dropdownElement.speed += score / 100
   }
   ctx.font = '20px Arial'
-  ctx.fillText(score, 350, 20)
+  ctx.fillText(score, canvas.width - 30, 30)
   mainElement.update();
   dropdownElement.update();
   requestAnimationFrame(drawFrame);
@@ -63,13 +125,6 @@ function drawFrame() {
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
-
-window.onkeydown = (event) => {
-  keyState[event.keyCode] = true;
-}
-window.onkeyup = (event) => {
-  keyState[event.keyCode] = false;
 }
 
 var startGame = confirm('Начать игру?')
